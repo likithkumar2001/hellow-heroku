c52b3879ab47022de8512c3112019b430daaa30a
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-07-19 14:09:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,42 @@ import urllib
 import dns
 app = Flask(__name__)
 
-CONNECTION_STRING = 'mongodb+srv://likith:' + urllib.parse.quote("Rp-iA@c6!Nq45c4") + '@cluster0.ms0ap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-client = pymongo.MongoClient(CONNECTION_STRING)
-base = client.weather
-data = base.data
+def connection():
+  CONNECTION_STRING = 'mongodb+srv://likith:' + urllib.parse.quote("Rp-iA@c6!Nq45c4") + '@cluster0.ms0ap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  client = pymongo.MongoClient(CONNECTION_STRING)
+  base = client.weather
+  data = base.data
+  client.close()
+  return data
+
+def fnd(x):
+  data = connection();
+  mydoc = data.find(x)
+  x = "."
+  for y in mydoc:
+    x=y
+  return x
+
 @app.route("/")
 def home():
-  return 'Connected to MONGODB!!!'
+  return 'Welcome change the url by adding /data_for?place=NAME !!!'
+
 @app.route("/data_for")
 def place():
   plc = request.args.get("place")
   x = {'title': plc}
-  mydoc = data.find(x) 
-  x = "."
-  for y in mydoc:
-    x=y
-  return x['Message']
+  val = fnd(x)
+  IST = pytz.timezone('Asia/Kolkata')
+  now = datetime.now(IST)
+  current_time = now.strftime("%H.%M")
+  if  val['Valid upto'] <= float(current_time):
+    u = data_accumulation.web_scrapping();
+    info = data_accumulation.data_cleaning(u);
+    data_accumulation.store_data(info);
+  x = {'title': plc}
+  x = {'title': result}
+  val = fnd(x)
+  return val['Message']
   
 
 
