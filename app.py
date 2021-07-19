731f8aedce153d5eb664fd3cb9fd96dd0a50e6de
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-07-19 13:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask
-import db
+from flask_pymongo import pymongo
+import urllib
+import dns
 app = Flask(__name__)
 
-@app.route('/')
-def indes():
-  return 'hello world'
+CONNECTION_STRING = 'mongodb+srv://likith:' + urllib.parse.quote("Rp-iA@c6!Nq45c4") + '@cluster0.ms0ap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = pymongo.MongoClient(CONNECTION_STRING)
+base = client.weather
+data = base.data
+x = {'title': 'TINSUKIA'}
+mydoc = data.find(x) 
+x = "."
+for y in mydoc:
+  x=y
+@app.route("/")
+def home():
+    retuen 'Connected to MONGODB!!!'
 
-@app.route("/test")
-def test():
-  f=db.mydoc 
-  x = "."
-  for y in f:
-    x=y
-  return x
+    
+
