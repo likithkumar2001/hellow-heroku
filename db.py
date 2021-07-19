03867b3b433f040a773e3423b3f1c363fd0bdf8b
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-07-19 11:26:53 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_pymongo import pymongo
 import urllib
+import dns
 CONNECTION_STRING = 'mongodb+srv://likith:' + urllib.parse.quote("Rp-iA@c6!Nq45c4") + '@cluster0.ms0ap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 client = pymongo.MongoClient(CONNECTION_STRING)
 base = client.weather
