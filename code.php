c1f84592802bb2d65c91e06503869cfbf63f0107
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-07-19 08:07:04 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,10 +1,4 @@
 <?php
-$conn = mysqli_connect("localhost","id17269055_likith","xjjh8cHA5%is!q|","id17269055_command");
-if ( !$conn ){
-echo "Not Connected...";
-}
-else{
-echo "Connected...";
 
-}
+echo "Connected...";
 ?>
